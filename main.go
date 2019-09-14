0c011a622310e627033850f8e53dd155c0703bba
Riyaz <khanriyaz24@gmail.com>
2019-09-14 23:18:39 +0400

    connected to mongodb and added query to add student

--- main.go
+++ main.go
@@ -1,27 +1,34 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Student struct {
-	ID         int    "json:id"
-	FirstName  string "json:firstname"
-	LastName   string "json:lastname"
-	FatherName string "json:fathername"
+	ID         int    `json:"_id,omitempty" bson:"_id,omitempty"`
+	FirstName  string `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	LastName   string `json:"lastname,omitempty" bson:"lastname,omitempty"`
+	FatherName string `json:"fathername,omitempty" bson:"fathername,omitempty"`
 }
 
 var students []Student
 
+// Connect to MongoDB
+var client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+var collection = client.Database("sahara").Collection("students")
+
 func getStudents(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	students, err = collection.Find()
 	json.NewEncoder(w).Encode(students)
 }
 func getStudent(w http.ResponseWriter, r *http.Request) {
@@ -36,20 +43,22 @@ func getStudent(w http.ResponseWriter, r *http.Request) {
 func addStudent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var student Student
-	_ = json.NewDecoder(r.Body).Decode(&student)
-	student.ID = rand.Intn(10000)
-	students = append(students, student)
-	json.NewEncoder(w).Encode(student)
+	json.NewDecoder(r.Body).Decode(&student)
+	result, err := collection.InsertOne(context.TODO(), student)
+	// students = append(students, student)
+	fmt.Println(err)
+	json.NewEncoder(w).Encode(result)
 }
 func main() {
 	name := "Shahnawaz Khan"
 	fmt.Println(name)
-	students = append(students, Student{ID: 2, FirstName: "Shahnawaz", LastName: "Khan", FatherName: "Shakeel Khan"})
-	students = append(students, Student{ID: 3, FirstName: "Asif", LastName: "Ansari", FatherName: "Uknown Khan"})
-
+	// students = append(students, Student{ID: 2, FirstName: "Shahnawaz", LastName: "Khan", FatherName: "Shakeel Khan"})
+	// students = append(students, Student{ID: 3, FirstName: "Asif", LastName: "Ansari", FatherName: "Uknown Khan"})
 	r := mux.NewRouter()
 	r.HandleFunc("/api/students/", getStudents).Methods("GET")
 	r.HandleFunc("/api/students/{id}", getStudent).Methods("GET")
 	r.HandleFunc("/api/students/add", addStudent).Methods("POST")
+
 	log.Fatal(http.ListenAndServe(":8001", r))
+
 }
